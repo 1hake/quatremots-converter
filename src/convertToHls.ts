a521e2eb2f0f654b2a276c9541d2aa3fabc46629
Your Name <you@example.com>
2024-10-17 10:45:31 +0200

    rebase

--- src/convertToHls.ts
+++ src/convertToHls.ts
@@ -1,29 +1,23 @@
-import { S3 } from '@aws-sdk/client-s3'
-import { spawn } from 'child_process'
-import path from 'path'
-import 'dotenv/config'
-import { fileURLToPath } from 'url'
-import { mkdirSync } from 'fs'
-import { v4 as uuid } from 'uuid'
+import { S3, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { spawn } from "child_process";
+import path from "path";
+import "dotenv/config";
+import { fileURLToPath } from "url";
+import { mkdirSync } from "fs";
+import { v4 as uuid } from "uuid";
+import express from "express";
+import cors from "cors";
 
 // Obtenir l'équivalent de __dirname
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const ENDPOINT = process.env.SCW_ENDPOINT as string
-const BUCKET_NAME = process.env.SCW_BUCKET_NAME as string
-const S3_REGION = process.env.SCW_REGION as string
-const ACCESS_KEY_ID = process.env.SCW_ACCESS_KEY as string
-const SECRET_KEY = process.env.SCW_SECRET_KEY as string
-
-console.log({
-    endpoint: ENDPOINT,
-    region: S3_REGION,
-    credentials: {
-        accessKeyId: ACCESS_KEY_ID,
-        secretAccessKey: SECRET_KEY,
-    },
-})
+const ENDPOINT = process.env.SCW_ENDPOINT as string;
+const BUCKET_NAME = process.env.SCW_BUCKET_NAME as string;
+const S3_REGION = process.env.SCW_REGION as string;
+const ACCESS_KEY_ID = process.env.SCW_ACCESS_KEY as string;
+const SECRET_KEY = process.env.SCW_SECRET_KEY as string;
 
 // Create S3 service object
 const s3 = new S3({
@@ -33,110 +27,127 @@ const s3 = new S3({
         accessKeyId: ACCESS_KEY_ID,
         secretAccessKey: SECRET_KEY,
     },
-})
+});
 
 // Définir les résolutions et les paramètres
 const resolutions = [
     {
-        name: '1080p',
-        bitrate: '5000k',
-        maxrate: '5350k',
-        bufsize: '7500k',
+        name: "1080p",
+        bitrate: "5000k",
+        maxrate: "5350k",
+        bufsize: "7500k",
         width: 1920,
         height: 1080,
     },
     {
-        name: '720p',
-        bitrate: '3000k',
-        maxrate: '3210k',
-        bufsize: '4500k',
+        name: "720p",
+        bitrate: "3000k",
+        maxrate: "3210k",
+        bufsize: "4500k",
         width: 1280,
         height: 720,
     },
     {
-        name: '480p',
-        bitrate: '1500k',
-        maxrate: '1605k',
-        bufsize: '2250k',
+        name: "480p",
+        bitrate: "1500k",
+        maxrate: "1605k",
+        bufsize: "2250k",
         width: 854,
         height: 480,
     },
     {
-        name: '140p',
-        bitrate: '500k',
-        maxrate: '600k',
-        bufsize: '750k',
+        name: "140p",
+        bitrate: "500k",
+        maxrate: "600k",
+        bufsize: "750k",
         width: 256,
         height: 140,
     },
-]
+];
 
-;(async () => {
-    const folderKey = uuid()
-    const folderPath = `${__dirname}/${folderKey}/`
+(async () => {
+    const folderKey = uuid();
+    const folderPath = `${__dirname}/${folderKey}/`;
     try {
-        mkdirSync(folderPath)
+        mkdirSync(folderPath);
     } catch (e) {
-        console.error(`Could not create folder path : ${folderPath}`)
+        console.error(`Could not create folder path : ${folderPath}`);
     }
 
-    const promises = resolutions.map(
-        ({ name, bitrate, maxrate, bufsize, width, height }) => {
-            return new Promise((resolve, reject) => {
-                const ffmpeg = spawn('ffmpeg', [
-                    '-i',
-                    'https://4mots-live.s3.fr-par.scw.cloud/input.mov',
-                    '-filter_complex',
-                    `[0:v]scale=w=${width}:h=${height}:force_original_aspect_ratio=decrease[vout]`,
-                    '-map',
-                    '[vout]',
-                    '-map',
-                    '0:a',
-                    '-c:a',
-                    'aac',
-                    '-ar',
-                    '48000',
-                    '-c:v',
-                    'h264',
-                    '-profile:v',
-                    'high',
-                    '-crf',
-                    '20',
-                    '-sc_threshold',
-                    '0',
-                    '-g',
-                    '48',
-                    '-keyint_min',
-                    '48',
-                    '-hls_time',
-                    '6',
-                    '-hls_playlist_type',
-                    'vod',
-                    '-b:v',
-                    bitrate,
-                    '-maxrate',
-                    maxrate,
-                    '-bufsize',
-                    bufsize,
-                    '-hls_segment_filename',
-                    path.join(folderPath, `${name}_%03d.ts`), // Segments stockés dans le répertoire courant
-                    '-f',
-                    'hls',
-                    path.join(folderPath, `${name}.m3u8`), // Playlist HLS stockée dans le répertoire courant
-                ])
-
-                ffmpeg.on('close', () => {
-                    resolve(true)
-                })
-                ffmpeg.on('error', () => {
-                    reject()
-                })
-            })
-        },
-    )
+    const promises = resolutions.map(({ name, bitrate, maxrate, bufsize, width, height }) => {
+        return new Promise((resolve, reject) => {
+            const ffmpeg = spawn("ffmpeg", [
+                "-i",
+                "https://4mots-live.s3.fr-par.scw.cloud/IMG_1184.MOV",
+                "-filter_complex",
+                `[0:v]scale=w=${width}:h=${height}:force_original_aspect_ratio=decrease[vout]`,
+                "-map",
+                "[vout]",
+                "-map",
+                "0:a",
+                "-c:a",
+                "aac",
+                "-ar",
+                "48000",
+                "-c:v",
+                "h264",
+                "-profile:v",
+                "high", // Keep high profile for 8-bit encoding
+                "-pix_fmt",
+                "yuv420p", // Force 8-bit color depth
+                "-crf",
+                "20",
+                "-sc_threshold",
+                "0",
+                "-g",
+                "48",
+                "-keyint_min",
+                "48",
+                "-hls_time",
+                "6",
+                "-hls_playlist_type",
+                "vod",
+                "-b:v",
+                bitrate,
+                "-maxrate",
+                maxrate,
+                "-bufsize",
+                bufsize,
+                "-hls_segment_filename",
+                path.join(folderPath, `${name}_%03d.ts`),
+                "-f",
+                "hls",
+                path.join(folderPath, `${name}.m3u8`),
+            ]);
+            // Capture ffmpeg stderr to log errors
+            ffmpeg.stderr.on("data", (data) => {
+                console.error(`ffmpeg stderr: ${data}`);
+            });
+            ffmpeg.on("close", () => {
+                resolve(true);
+            });
+            ffmpeg.on("error", (err) => {
+                console.log(err);
+                reject(err);
+            });
+            ffmpeg.on("exit", (t, e) => {
+                console.log(t, e);
+            });
+        });
+    });
     try {
-        await Promise.all(promises)
+        console.time("execition");
+
+        await Promise.all(promises);
+        console.timeEnd("execition");
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
-})()
+})();
+
+const app = express();
+app.use(cors()).use(express.json());
+
+app.post("/handler", (req, res) => {
+    const { objectKey } = req.body;
+});
